c6a2fc20d27fd0d2e523e0c98b751f2f5699cff6
Kevin <kevinmoore9@users.noreply.github.com>
2017-02-06 07:07:35 -0800

    update deposit controller

--- app/controllers/api/deposits_controller.rb
+++ app/controllers/api/deposits_controller.rb
@@ -1,8 +1,8 @@
 class Api::DepositsController < ApplicationController
 
   def create
-    @user = User.find_by(id: params[:id])
-    @user.deposit_money(amount)
+    @user = User.find_by(id: params[:user_id])
+    @user.deposit_money(params[:user][:amount])
     render "api/users/show"
   end
 
