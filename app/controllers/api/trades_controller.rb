71b55c564c14ccd3a2e714fb3f87622fd3741e4f
Kevin <kevinmoore9@users.noreply.github.com>
2017-02-06 12:36:12 -0800

    update trade controller

--- app/controllers/api/trades_controller.rb
+++ app/controllers/api/trades_controller.rb
@@ -4,9 +4,9 @@ class Api::TradesController < ApplicationController
     if ['BUY', 'SELL'].include?(trade_params[:trade_type])
       @user = User.find_by(id: trade_params[:user_id])
       if trade_params[:trade_type] == "BUY"
-        user.buy_stock(trade_params[:ticker_sym], trade_params[:volume])
+        @user.buy_stock(trade_params[:ticker_sym], trade_params[:volume])
       else # must be SELL
-        user.sell_stock(trade_params[:ticker_sym], trade_params[:volume])
+        @user.sell_stock(trade_params[:ticker_sym], trade_params[:volume])
       end
       render "api/users/show"
     else
